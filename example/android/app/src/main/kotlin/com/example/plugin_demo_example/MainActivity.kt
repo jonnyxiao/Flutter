cda2d246cf696dfbd8bebc353ec27fa3c9fbcb43
jinxiao <liangxiangyang.co>
2022-05-25 15:59:40 +0800

    首次提交

--- example/android/app/src/main/kotlin/com/example/plugin_demo_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.plugin_demo_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- ios/Classes/PluginDemoPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface PluginDemoPlugin : NSObject<FlutterPlugin>
-@end
